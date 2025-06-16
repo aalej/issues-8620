bd524a52c34d5481e144b002d68fb6b542a0bdbd
aalej <alejandromarco@google.com>
2025-06-17 04:35:07 +0800

    Add default valud for STORAGE_BUCKET

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,17 +1,18 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  console.log(request)
-  // Do whatever you want
+  console.log(request);
   return NextResponse.json(
-    { message: "Hello World", storageBucket: process.env.STORAGE_BUCKET },
+    {
+      message: "Hello World",
+      storageBucket: process.env.STORAGE_BUCKET ?? "_blank",
+    },
     { status: 200 }
   );
 }
 
 // To handle a POST request to /api
 export async function POST(request: Request) {
-  console.log(request)
-  // Do whatever you want
+  console.log(request);
   return NextResponse.json({ message: "Hello World" }, { status: 200 });
 }
