a2649623687250f6cff66b293a533bc5ea76e2ef
aalej <alejandromarco@google.com>
2025-06-17 05:12:40 +0800

    Added app version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+// This code runs ONLY on the server during the Next.js build process.
+// process.env.APP_VERSION is available here because it's a private env variable.
+const appVersion = process.env.APP_VERSION || "N/A";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -21,7 +25,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Save and see your changes instantly. App Version: {appVersion}
             {process.env.NEXT_PUBLIC_MESSAGE}
           </li>
         </ol>
